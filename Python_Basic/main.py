962a5d87e1a73abccc7e0e0caca61ed699ff3242
Pradeep Tamta <118155647+Pradeeptamta3225@users.noreply.github.com>
2025-02-28 10:40:43 +0000

    learn about random number

--- Python_Basic/main.py
+++ Python_Basic/main.py
@@ -97,3 +97,14 @@ bmi = weight / height
             print("You got attacked by angry trout, game Over\n")
     else:
         print("You fell into a hole.Game over")
+
+
+
+
+import random
+
+random_integer = random.randint(0,1)
+if random_integer == 0:
+    print("Head")
+else:
+    print("Tail")
